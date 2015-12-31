3550ad795aa71e16a29e652a2b7cadb32ddd8825
Rene Penner <rene@penner.name>
2015-12-31 15:04:57 +0100

    add Container to test

--- app/tests/ProductTest.php
+++ app/tests/ProductTest.php
@@ -2,12 +2,25 @@
 namespace RenePenner\StateLessShop\Tests\Core;
 
 use PHPUnit_Framework_TestCase;
+use Pimple\Container;
 use RenePenner\StateLessShop\Core\Number\Integer;
 use RenePenner\StateLessShop\Core\Product\Product;
 use RenePenner\StateLessShop\Core\Product\ProductRepositoryMemory;
 
 class ProductTest extends PHPUnit_Framework_TestCase
 {
+
+    /**
+     * @var Container
+     */
+    protected $container;
+
+    protected function setUp()
+    {
+        $this->container = new Container();
+        $this->container['ProductRepository'] = new ProductRepositoryMemory();
+    }
+
     /**
      * @test
      */
@@ -24,7 +37,7 @@ class ProductTest extends PHPUnit_Framework_TestCase
      */
     public function addProductToProductRepository()
     {
-        $productRepository = new ProductRepositoryMemory();
+        $productRepository = $this->container['ProductRepository'];
 
         $product = new Product(new Integer(1));
         $product->setName('ProductA');
